4ed5178d1162c1f616dbe9dc21ed3cff40ad133c
German Roman <psi.germanr@gmail.com>
2024-10-01 15:17:54 -0300

    feat: add i18next

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '../i18n.ts';
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
