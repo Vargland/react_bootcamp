c5ed4473cc82d7716d5dde5e19451c0ab5cdfa6d
German Roman <psi.germanr@gmail.com>
2024-10-02 00:00:16 -0300

    refactor: remove extra code

--- src/services/config/client.config.ts
+++ src/services/config/client.config.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 import { API } from "../../constants/api";
 
-
 export const client = axios.create({
-    method: 'get',
     baseURL: API.BASE,
 });
-
 
 client.interceptors.response.use(
     function (response) {
